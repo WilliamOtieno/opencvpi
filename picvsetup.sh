02a45ed1c0d328a7b159ae64e28616c6841aa76e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-11-20 11:26:42 +0300

    Added functionality to setup script

--- picvsetup.sh
+++ picvsetup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "Updating and upgrading your Pi..."
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo rpi-update -y
 sudo echo "CONF_SWAPSIZE=2048" > /etc/dphys-swapfile
+echo "Installing all required dependencies..."
 sudo apt-get install build-essential cmake pkg-config -y
 sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
 sudo apt-get install libxvidcore-dev libx264-dev -y
 sudo apt-get install libgtk2.0-dev libgtk-3-dev -y
 sudo apt-get install libatlas-base-dev gfortran -y
+echo "Going back to the home directory..."
+cd ~
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.4.0.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.4.0.zip
 unzip opencv.zip
@@ -16,6 +20,7 @@ sudo pip3 install numpy
 cd ~/opencv-4.4.0
 mkdir build
 cd build
+echo "Initiating build process..."
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
   -D CMAKE_INSTALL_PREFIX=/usr/local \
   -D INSTALL_PYTHON_EXAMPLES=ON \
