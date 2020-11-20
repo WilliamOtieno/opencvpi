990173027c147148c2c75e527030eaf20652b15d
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-11-20 04:08:15 +0300

    modified test script

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-import cv2
-import time
+try:
+    import cv2
+    import time
 
-print("Importing OpenCV-Python...")
-print("Module loaded successfully")
-time.sleep(2)
-print(f"OpenCV-Python Version: {cv2.__version__}")
-time.sleep(5)
-print("Module is ready for use.")
-time.sleep(2)
-exit()
+    print("Importing OpenCV-Python...")
+    print("Module loaded successfully")
+    time.sleep(2)
+    print(f"OpenCV-Python Version: {cv2.__version__}")
+    time.sleep(5)
+    print("Module is ready for use.")
+    time.sleep(2)
+    exit()
+except ModuleNotFoundError:
+    print("It seems opencv-python is not installed in your system.")
+    print("Kindly rerun the setup script.")
